95729260950d7305531601820493ec1573f5a1be
mjescalante <122808004+mjescalante@users.noreply.github.com>
2024-02-05 10:28:52 +0100

    Versión María Moodle

---
+++ GestorFacturas/src/main/java/es/torredelrey/gestorfacturas/GestorFacturas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.torredelrey.gestorfacturas;
+
+import es.torredelrey.gestorfacturas.vista.LogIn;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GestorFacturas {
+
+    public static void main(String[] args) {
+        LogIn lg = new LogIn();
+        lg.show(true);
+    }
+}
