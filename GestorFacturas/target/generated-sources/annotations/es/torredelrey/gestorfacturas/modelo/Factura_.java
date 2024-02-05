6341ea3d1467f892c0556006421ccd67872ba3ee
María Netbai <netbaimaria@gmail.com>
2024-02-05 11:21:05 +0100

    Updating

--- GestorFacturas/target/generated-sources/annotations/es/torredelrey/gestorfacturas/modelo/Factura_.java
+++ GestorFacturas/target/generated-sources/annotations/es/torredelrey/gestorfacturas/modelo/Factura_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-01-31T18:39:43", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-05T11:20:05", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Factura.class)
 public class Factura_ { 
 
